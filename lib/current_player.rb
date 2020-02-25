c453091191f909702e5721fea3a1a2201b4777e7
Sarah Rose <sarahrose63089@gmail.com>
2020-02-25 23:32:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
   counter = 0
   board.each do |index|
-    index == "X" || index == "O"
-    counter += 1
+    if index == "X" || index == "O"
+      counter += 1
+    end
   end
 end
